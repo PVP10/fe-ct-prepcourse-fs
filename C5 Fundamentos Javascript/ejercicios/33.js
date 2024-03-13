67815eca18788e67c64fa014bf7214f8451bca55
Paula Pucharella <paulavpch@gmail.com>
2024-03-13 19:35:09 -0300

    resolucion ej M5

--- C5 Fundamentos Javascript/ejercicios/33.js
+++ C5 Fundamentos Javascript/ejercicios/33.js
@@ -6,6 +6,16 @@ function esNumeroPrimo(numero) {
   // IMPORTANTE: Recuerda que un número primo es aquel que
   // solo es divisible por sí mismo y por 1.
   // Tu código:
+  if(numero <= 1) {
+    return false; // Los números menores o iguales a 1 no son primos
+  }
+  for(var i = 2; i < numero; i++) {
+    if(numero % i === 0) {
+      return false; // Si el número es divisible por algún número entre 2 y numero-1, entonces no es primo
+    }
+  }
+  return true; // Si no se encontró ningún divisor entre 2 y numero-1, entonces es primo
+
 }
 
 module.exports = esNumeroPrimo;
